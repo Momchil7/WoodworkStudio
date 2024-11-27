d05ada53f7e08262eb51ea6de88ce8f93eff050a
Momchil7 <133247829+Momchil7@users.noreply.github.com>
2024-11-27 19:15:05 +0200

    Initial commit

---
+++ content/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+# urlpatterns = [
+#     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+#     path('projects/create/', views.ProjectCreateView.as_view(), name='create-project'),
+#     path('projects/<int:pk>/edit/', views.ProjectEditView.as_view(), name='edit-project'),
+#     path('projects/<int:pk>/delete/', views.ProjectDeleteView.as_view(), name='delete-project'),
+#     path('projects/<int:pk>/', views.ProjectDetailView.as_view(), name='details-project'),
+#     path('tutorials/create/', views.TutorialCreateView.as_view(), name='create-tutorial'),
+#     path('tutorials/<int:pk>/edit/', views.TutorialEditView.as_view(), name='edit-tutorial'),
+#     path('tutorials/<int:pk>/delete/', views.TutorialDeleteView.as_view(), name='delete-tutorial'),
+#     path('tutorials/<int:pk>/', views.TutorialDetailView.as_view(), name='details-tutorial'),
+# ]
